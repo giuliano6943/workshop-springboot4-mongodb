f091ecc37c22d0bcb5dca1d89a880f553fdf4e75
Giuliano Neto <Giuliano_neto@outlook.com>
2026-01-27 11:32:22 -0300

    Obtendo um usuário por id

--- src/main/java/com/giuliano/workshopmongo/services/UserService.java
+++ src/main/java/com/giuliano/workshopmongo/services/UserService.java
@@ -1,11 +1,14 @@
 package com.giuliano.workshopmongo.services;
 
 import com.giuliano.workshopmongo.domain.User;
+import com.giuliano.workshopmongo.dto.UserDTO;
 import com.giuliano.workshopmongo.repository.UserRepository;
+import com.giuliano.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +18,10 @@ public class UserService {
 
     public List<User> findAll(){
         return repo.findAll();
+    }
+
+    public User findById(String id){
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
---
+++ src/main/java/com/giuliano/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.giuliano.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+    public ObjectNotFoundException(String msg) {
+        super(msg);
+    }
+
+}
