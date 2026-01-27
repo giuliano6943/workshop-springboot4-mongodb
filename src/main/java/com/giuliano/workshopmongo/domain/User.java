75c1bee5243efbcdbe7c1785f874dc8ec9894a70
Giuliano Neto <Giuliano_neto@outlook.com>
2026-01-27 11:07:26 -0300

    Operacao de instanciacao da base de dados

--- src/main/java/com/giuliano/workshopmongo/domain/User.java
+++ src/main/java/com/giuliano/workshopmongo/domain/User.java
@@ -1,33 +1,32 @@
 package com.giuliano.workshopmongo.domain;
 
-import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
 import java.util.Objects;
 
-@Document
+@Document(collection = "user")
 public class User implements Serializable {
 
     @Id
-    private ObjectId id;
+    private String id;
     private String name;
     private String email;
 
     public User() {}
 
-    public User(ObjectId id, String name, String email) {
+    public User(String id, String name, String email) {
         this.id = id;
         this.name = name;
         this.email = email;
     }
 
-    public ObjectId getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(ObjectId id) {
+    public void setId(String id) {
         this.id = id;
     }
 
